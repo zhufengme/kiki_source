637ccfc7dc55d2ae5b0c639620ffbf339adedc14
Andie Zhu <i@zhufeng.me>
2016-01-05 15:37:59 +0800

    modify

--- controllers/dtest.class.php
+++ controllers/dtest.class.php
@@ -4,8 +4,10 @@ namespace controllers;
 class dtest extends \console{
     public function test(){
 
-        $obj = new \models\dtest();
-        $obj->test();
+        //$obj = new \models\dtest();
+        //$obj->test();
+
+        $this->output->out("test","red");
 
 
         //put code here.
