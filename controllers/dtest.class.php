1cc905f6c977aff6e1ba3a2f594fb733579e584a
Andie Zhu <i@zhufeng.me>
2016-01-22 16:37:42 +0800

    add db assoc fetch

--- controllers/dtest.class.php
+++ controllers/dtest.class.php
@@ -4,10 +4,10 @@ namespace controllers;
 class dtest extends \console{
     public function test(){
 
-        //$obj = new \models\dtest();
-        //$obj->test();
+        $obj = new \models\dtest();
+        $obj->test();
 
-        $this->output->out("test","red");
+        //$this->output->out("test","red");
 
 
         //put code here.
